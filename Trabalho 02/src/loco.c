8dba02dc64f64fe83d47c4f98cc6523b06e639ba
Elton Cardoso do Nascimento <eltsu3110@hotmail.com>
2021-01-01 20:47:41 -0300

    Tentativa de controle longitudinal

--- Trabalho 02/src/loco.c
+++ Trabalho 02/src/loco.c
@@ -1,12 +1,87 @@
 #include "api_car.h"
 
+
+float kp = 0.001;
+float ki = 0;
+float kd = 0;
+
+float setpointVel = 100;
+
+float posicao[3];
+float direcao[3];
+float velocidade[3];
+float magVelocidade; //AINDA PRECISA CRIAR FUNÇÃO PARA CALCULAR RAIZ, POSSUI MAG^2
+float deltaT;
+float ultimaPosicao[3];
+int accel;
+
+
+void controle_longitudinal()
+{
+    float erro = setpointVel - magVelocidade;
+
+    float saida = kp*erro;
+
+    if(saida > 0)
+    {
+        accel = 1;
+    }
+    else if(saida < 0)
+    {
+        accel = -1;
+    }
+    else
+    {
+        accel = 0;
+    }
+}
+
+void calcula_velocidade()
+{
+    velocidade[0] = posicao[0] - ultimaPosicao[0];
+    velocidade[1] = posicao[1] - ultimaPosicao[1];
+    velocidade[2] = posicao[2] - ultimaPosicao[2];
+
+    velocidade[0] /= deltaT;
+    velocidade[1] /= deltaT;
+    velocidade[2] /= deltaT;
+
+    magVelocidade = 0;
+    magVelocidade += velocidade[0] * velocidade[0];
+    magVelocidade += velocidade[1] * velocidade[1];
+    magVelocidade += velocidade[2] * velocidade[2];
+ }
+
 // Implemente aqui a lógica de controle do carro, utilizando as funções da api
 void user_code(void) 
 {
+    unsigned int time = get_time();
 
-    while(read_sensor_distance()>10 || read_sensor_distance() < 0)
+    unsigned int ultimoTime = time;
+
+    while(get_time()-time< (30*1000))
     {
-        set_motor(1,0);
+        int x, y, z;
+        get_position(&x, &y, &z);
+
+        posicao[0] = ((float) x)/10.0;
+        posicao[1] = ((float) y)/10.0;
+        posicao[2] = ((float) z)/10.0;
+
+        deltaT = get_time() - ultimoTime;
+        deltaT /= 1000.0;
+
+        calcula_velocidade();
+
+        controle_longitudinal();
+
+
+        set_motor(accel, 0);
+
+        ultimaPosicao[0] = posicao[0];
+        ultimaPosicao[1] = posicao[1];
+        ultimaPosicao[2] = posicao[2];
+        ultimoTime = get_time();
     }
 
     set_motor(0, 0);
