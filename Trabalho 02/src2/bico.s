7f9462fa145470eb52eb069faecb05ef36a1802b
Elton Cardoso do Nascimento <eltsu3110@hotmail.com>
2021-01-13 14:52:27 -0300

    Entrega1

---
+++ Trabalho 02/src2/bico.s
@@ -0,0 +1,109 @@
+.globl set_motor
+.globl set_handbreak
+.globl read_sensors
+.globl read_sensor_distance
+.globl get_time
+.globl get_position
+.globl get_rotation
+
+# Implemente aqui as funções da API_CAR.
+# As funções devem checar os parametros e fazer as chamadas de sistema que serão
+#   tratadas na camada SoCa
+
+set_motor:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+
+    li a7, 10
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
+
+set_handbreak:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+
+    li a7, 11
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
+    
+read_sensors:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+    
+    li a7, 12
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
+
+read_sensor_distance:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+
+    li a7, 13
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
+
+get_position:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+
+    li a7, 15
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
+
+get_rotation:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+
+    li a7, 16
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
+
+get_time:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw fp, 4(sp)
+    addi fp, sp, 16
+
+    li a7, 14
+    ecall
+
+    lw ra, 0(sp)
+    lw fp, 4(sp)
+    addi sp, sp, 16
+    ret
